9cd9d13681f0c3c7529cfb1f9266627dbeb02ffe
arochet <albanrochet@gmail.com>
2018-11-14 15:21:12 +0100

    Creation de toutes les activités principales

--- app/src/main/java/com/example/rochet/eatsmart/ScanActivity.java
+++ app/src/main/java/com/example/rochet/eatsmart/ScanActivity.java
@@ -3,12 +3,11 @@ package com.example.rochet.eatsmart;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ScanActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //Page de bienvenue
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_scan);
     }
 }
