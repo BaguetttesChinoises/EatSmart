2a88e60185448de0c1543086ac1ee3270aa69076
arochet <albanrochet@gmail.com>
2018-11-14 15:30:10 +0100

    On peut passer de la page d'accueil à la page Scan

--- app/src/main/java/com/example/rochet/eatsmart/MainActivity.java
+++ app/src/main/java/com/example/rochet/eatsmart/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.rochet.eatsmart;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonCommencer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //Page de bienvenue
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonCommencer = findViewById(R.id.buttonCommencer);
+    }
+
+    void goToScanActivity(View view) {
+        Intent intent = new Intent(this, ScanActivity.class);
+        startActivity(intent);
     }
 }
